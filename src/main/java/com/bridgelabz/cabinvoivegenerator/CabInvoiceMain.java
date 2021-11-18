6199150ee6bd986697f337fd7307e2df88858961
Your Name <you@example.com>
2021-11-18 18:23:38 +0530

    Initial Commit

---
+++ src/main/java/com/bridgelabz/cabinvoivegenerator/CabInvoiceMain.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.cabinvoivegenerator;
+
+public class CabInvoiceMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Invoice Gen.");
+    }
+}
